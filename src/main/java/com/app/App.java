ce2c723bea2f9b69b8671f4355ef13ede74e769a
Jalal Kiswani <kiswanij@gmail.com>
2023-06-21 15:44:48 +0300

    UPDATE TO VERSOIN 7.0.0-SNAPSHOT

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,5 +1,6 @@
 package com.app;
 
+import com.jk.core.cache.JKCacheFactory;
 import com.jk.services.server.JKServiceConfig;
 import com.jk.web.embedded.JKWebApplication;
 
@@ -13,6 +14,10 @@ public class App extends JKServiceConfig{
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		JKCacheFactory.getCacheManager().cache("Key1", "Jalal1");
+		JKCacheFactory.getCacheManager().cache("Key2", "Jalal2");
+		JKCacheFactory.getCacheManager().cache("Key3", "Jalal3");
+		JKCacheFactory.getCacheManager().cache("Key4", 10, Integer.class);
 		JKWebApplication.run(8080,false);
 	}
 
